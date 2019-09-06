d796fd13e42baae5ea77d183ecfb5309901d9bac
Ubuntu <ubuntu@ip-172-31-18-207.ec2.internal>
2019-09-06 07:36:40 +0000

    Added staticfiles folder

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -76,7 +76,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Auth
