c30b373ee432d8664bd430bce4635d043f4223f4
Ubuntu <ubuntu@ip-172-31-18-207.ec2.internal>
2019-09-07 08:11:57 +0000

    Ready to deploy again

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 from .models import Item, OrderItem, Order, Payment, Coupon, Refund, Address, UserProfile
-from .models import Photo
 
 def make_refund_accepted(modeladmin, request, queryset):
     queryset.update(refund_requested=False, refund_granted=True)
@@ -63,4 +62,3 @@ admin.site.register(Coupon)
 admin.site.register(Refund)
 admin.site.register(Address, AddressAdmin)
 admin.site.register(UserProfile)
-admin.site.register(Photo)
